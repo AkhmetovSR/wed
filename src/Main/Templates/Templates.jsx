eb0936c9cc39394fdcc4ee87daf86569ce33084a
ahmetov <ahmetov26@gmail.com>
2025-02-16 22:06:28 +0500

    home 16 02 2025

--- src/Main/Templates/Templates.jsx
+++ src/Main/Templates/Templates.jsx
@@ -5,13 +5,11 @@ import {AnimatePresence, motion} from "framer-motion";
 import {useLocation} from "react-router-dom";
 
 export default function Templates() {
-
     return (
         <AnimatePresence>
-            <motion.div className={s.Templates} initial={{scale:1.2}} animate={{scale:1}} exit={{scale:1.2}} transition={{duration:0.1}}>
+            <motion.div className={s.Templates} initial={{scale: 1.2}} animate={{scale: 1}} exit={{scale: 1.2}} transition={{duration: 0.1}}>
                 <Carousel/>
             </motion.div>
         </AnimatePresence>
-
     );
 }
